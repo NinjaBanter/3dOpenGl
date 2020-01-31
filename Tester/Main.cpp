69b590482350d71c8fae39a588b19a126c4cb7ff
NinjaBanter <Markfeeney91@hotmail.com>
2020-01-31 20:08:57 +0000

    Add project files.

---
+++ Tester/Main.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include <iostream>
+#include <glew.h>
+#include "display.h"
+
+int main(int argc, char *args[])
+{
+	Display display(800, 600, "Hello World!");
+
+	while (!display.IsClosed())
+	{
+		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+		display.Update();
+	}
+
+
+	return 0;
+}
---
+++ Tester/display.h
@@ -0,0 +1,32 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#pragma once
+
+#include <SDL.h>
+#include <iostream>
+
+
+class Display
+
+
+{
+public:
+	Display(int width, int height, const std::string& title);
+
+	void Clear(float r, float g, float b, float a);
+	void Update();
+	bool IsClosed();
+
+	virtual ~Display();
+
+protected:
+
+private:
+	SDL_Window* m_window;
+	SDL_GLContext m_glContext;
+
+	bool m_isClosed;
+};
+
+#endif // !DISPLAY_H
+
